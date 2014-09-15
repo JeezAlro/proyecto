f7413759b854c38ca723c347782df16e43ebecf1
jeezalro <jesus.alberto.lopez@outlook.com>
2014-09-14 19:56:15 -0600

    Actulizacion
    jj

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,16 +1,93 @@
-	var $usurario = $('.movilusuario'),
-		$mostraru = $('.miavatar'),
-		$categorias = $('.movilcategorias'),
-		$mostrarcate = $('.menuMobil')
-
-function mostrarmu(e){
-	$usurario.slideToggle();
+	var $menuUser = $('.menuUser'),
+		$mostrarMenu = $('.miavatar'),
+		$categorias = $('.categoriasMobil'),
+		$mostrarCateg = $('.menuMobil'),
+		$mostrarMensajes = $('.icon-mail2'),
+		$mostrarNotificaciones = $('.icon-bell'),
+		$mostrarOrdenes = $('.icon-orden'),
+		$notimjs = $('.notimjs'),
+		$notinot = $('.notinot'),
+		$notiord = $('.notiord'),
+		$popmsm = $('.mjs'),
+		$popnot = $('.not'),
+		$popord = $('.ord')
+function mostrarMenuUser(e){
+	$popmsm.slideUp();
+	$popnot.slideUp();
+	$popord.slideUp();
+	$mostrarMensajes.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'transparent')
+	$mostrarNotificaciones.css('background', 'transparent')
+	$menuUser.slideToggle();
+	$categorias.slideUp();
 	return false
 }
-function mostrarmc(e){
+function mostrarCategorias(e){
+	$popmsm.slideUp();
+	$popnot.slideUp();
+	$popord.slideUp();
+	$mostrarMensajes.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'transparent')
+	$mostrarNotificaciones.css('background', 'transparent')
 	$categorias.slideToggle();
+	$menuUser.slideUp();
 	return false
 }
 
-$mostraru.click(mostrarmu)
-$mostrarcate.click(mostrarmc)
+function mostrarMjs(e){
+	$notimjs.addClass('none');
+	$menuUser.slideUp();
+	$categorias.slideUp();
+	$popnot.slideUp();
+	$popord.slideUp();
+	$popmsm.slideToggle();
+	$mostrarNotificaciones.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'transparent')
+	$mostrarMensajes.css('background', 'rgba(0,0,0,0.5)')
+	return false
+
+}
+function mostrarNot(e){
+	$notinot.addClass('none');
+	$menuUser.slideUp();
+	$categorias.slideUp();
+	$popmsm.slideUp();
+	$popord.slideUp();
+	$popnot.slideToggle();
+	$mostrarMensajes.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'transparent')
+	$mostrarNotificaciones.css('background', 'rgba(0,0,0,0.5)')
+
+	return false
+
+}
+function mostrarOrd(e){
+	$notiord.addClass('none');
+	$menuUser.slideUp();
+	$categorias.slideUp();
+	$popmsm.slideUp();
+	$popnot.slideUp();
+	$popord.slideToggle();
+	$mostrarMensajes.css('background', 'transparent')
+	$mostrarNotificaciones.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'rgba(0,0,0,0.5)')
+
+	return false
+
+}
+function ocultarMenus(e){
+	$menuUser.slideUp();
+	$categorias.slideUp();
+	$popmsm.slideUp();
+	$popnot.slideUp();
+	$popord.slideUp();
+	$mostrarMensajes.css('background', 'transparent')
+	$mostrarOrdenes.css('background', 'transparent')
+	$mostrarNotificaciones.css('background', 'transparent')
+}
+$(window).click(ocultarMenus)
+$mostrarMensajes.click(mostrarMjs)
+$mostrarMenu.click(mostrarMenuUser)
+$mostrarNotificaciones.click(mostrarNot)
+$mostrarOrdenes.click(mostrarOrd)
+$mostrarCateg.click(mostrarCategorias)
